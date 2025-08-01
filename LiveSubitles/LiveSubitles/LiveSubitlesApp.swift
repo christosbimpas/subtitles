48be004ec3db2863ede27441e891e10954c99bad
Christos Bimpas <christos.bimpas@icloud.com>
2025-08-01 16:23:44 +0300

    xcode project

---
+++ LiveSubitles/LiveSubitles/LiveSubitlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSubitlesApp.swift
+//  LiveSubitles
+//
+//  Created by Christos Bimpas on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct LiveSubitlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveSubitles/LiveSubitlesTests/LiveSubitlesTests.swift
@@ -0,0 +1,16 @@
+//
+//  LiveSubitlesTests.swift
+//  LiveSubitlesTests
+//
+//  Created by Christos Bimpas on 01/08/2025.
+//
+
+import Testing
+
+struct LiveSubitlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
