0157c9003bc84502ea40726279e6826a1b1de859
Christos Bimpas <christos.bimpas@icloud.com>
2025-08-01 12:15:22 +0300

    change project name

---
+++ LiveSubtitles/LiveSubtitlesTests/LiveSubtitlesTests.swift
@@ -0,0 +1,16 @@
+//
+//  LiveSubtitlesTests.swift
+//  LiveSubtitlesTests
+//
+//  Created by Christos Bimpas on 01/08/2025.
+//
+
+import Testing
+
+struct LiveSubtitlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
