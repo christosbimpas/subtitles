bbd03249e52e7d32c26a98de9683346de6bcc546
Christos Bimpas <christos.bimpas@icloud.com>
2025-08-01 16:38:45 +0300

    init Xcode project

---
+++ LiveSubtitles/LiveSubtitles/LiveSubtitlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSubtitlesApp.swift
+//  LiveSubtitles
+//
+//  Created by Christos Bimpas on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct LiveSubtitlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
